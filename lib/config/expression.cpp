b0022fbc735c765750fec15aea72b91f23396c78
Yonas Habteab <yonas.habteab@icinga.com>
2022-06-21 11:12:35 +0200

    Expression: Don't allow to create objects/templates with null or integer names

--- lib/config/expression.cpp
+++ lib/config/expression.cpp
@@ -949,16 +949,26 @@ ExpressionResult ObjectExpression::DoEvaluate(ScriptFrame& frame, DebugHint *dhi
 	CHECK_RESULT(typeres);
 	Type::Ptr type = typeres.GetValue();
 
-	String name;
+	Value value;
 
 	if (m_Name) {
 		ExpressionResult nameres = m_Name->Evaluate(frame, dhint);
 		CHECK_RESULT(nameres);
 
-		name = nameres.GetValue();
+		value = nameres.GetValue();
 	}
 
-	return VMOps::NewObject(frame, m_Abstract, type, name, m_Filter, m_Zone,
+	if (!m_DefaultTmpl) {
+		if (value.IsEmpty()) {
+			BOOST_THROW_EXCEPTION(ScriptError("Template/object name must not be empty", m_DebugInfo));
+		}
+
+		if (!value.IsString()) {
+			BOOST_THROW_EXCEPTION(ScriptError("Template/object name must be a string", m_DebugInfo));
+		}
+	}
+
+	return VMOps::NewObject(frame, m_Abstract, type, std::move(value), m_Filter, m_Zone,
 		m_Package, m_DefaultTmpl, m_IgnoreOnError, m_ClosedVars, m_Expression, m_DebugInfo);
 }
 
